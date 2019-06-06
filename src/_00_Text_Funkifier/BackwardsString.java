4b6f911867250a51bff9eb546473f8fa8b035e64
League Student <league@iMac-56.attlocal.net>
2019-06-05 20:01:57 -0700

    FunkifyText(IP)

--- src/_00_Text_Funkifier/BackwardsString.java
+++ src/_00_Text_Funkifier/BackwardsString.java
@@ -8,11 +8,13 @@ public class BackwardsString extends SpecialString {
 
 	@Override
 	public String funkifyText(String s) {
-		for(int i = 0; i < s.length()/2; i++) {
-			System.out.println(s.charAt(i));
-			char k = s.charAt(i);
-			s.replace(s.charAt(i), s.charAt(s.length()-1-i));
-			s.replace(s.charAt(s.length()-1-i), k);
+		char[] chars = s.toCharArray();
+		s = "";
+		for(int i = 0; i < s.length()/2-1 ; i++) { 
+			char k = chars[i];
+			chars[i] = chars[chars.length-1-i];
+			chars[chars.length-1-i] = k;
+			
 		}
 		return s;
 	}
